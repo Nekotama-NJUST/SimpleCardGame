6d54a02c937d52a5aa72ede7f2a5738a5f87924c
Nekotama-NJUST <150797418+Nekotama-NJUST@users.noreply.github.com>
2025-07-04 15:47:35 +0800

    Update hello_git.py冲突解决测试

--- hello_git.py
+++ hello_git.py
@@ -1,7 +1,7 @@
 # hello_git.py v2
 def greet(name):
     print(f"Hello {name}! Git is working well!")
-
+#冲突解决测试
 def main():
     greet("Developer")
     print("Congratulations! Your Git flow is complete!")
